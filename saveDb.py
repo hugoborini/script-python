6dbd10db89513a82086a82aa8b1b2ab734443d25
hugoborini <hugo.borini@hetic.net>
2021-05-27 12:39:44 +0200

    add

--- saveDb.py
+++ saveDb.py
@@ -3,9 +3,10 @@ import sys
 from datetime import datetime
 
 def saveDb(dataBaseName, date):
-    os.system(f"ssh hugo@52.169.79.53 sudo mysqldump -u root {dataBaseName} > {dataBaseName}_{date}.sql")
+    os.system(f"ssh hugo@52.169.79.53 sudo mysqldump -u root {dataBaseName} > saveDb/{dataBaseName}_{date}.sql")
 
 
 if __name__=="__main__":
     date = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
     saveDb(sys.argv[1], date)
+    os.system("ls")
