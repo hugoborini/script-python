47f1b8c50238cd8b0e16085bf3ca25b595698d52
hugoborini <hugo.borini@hetic.net>
2021-05-27 12:26:14 +0200

    refacto bddsave

--- bddSave.py
+++ bddSave.py
@@ -3,10 +3,9 @@ import sys
 from datetime import datetime
 
 def saveBdd(dataBaseName, date):
-    os.system(f"ssh hugo@52.169.79.53 sudo mysqldump -u root {dataBaseName} > {dataBaseName}-{date}.sql")
+    os.system(f"ssh hugo@52.169.79.53 sudo mysqldump -u root {dataBaseName} > {dataBaseName}_{date}.sql")
 
 
 if __name__=="__main__":
-
-    saveBdd(sys.argv[1], str(datetime.timestamp(datetime.now())))
-
+    date = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    saveBdd(sys.argv[1], date)
